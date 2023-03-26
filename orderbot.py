a67e7b269981590adb81034a1e7bf48a283014ec
seoxubuntu <icyveins@hotmail.com>
2023-03-26 23:45:44 +0800

    complete most of the order callback keyboards and logic
    need to finish the closing of order

--- orderbot.py
+++ orderbot.py
@@ -18,18 +18,23 @@ class OrderInterface:
         'Bandung': {'Normal'},
         'Can/Packet': {'Coke', 'Pepsi', 'Sprite', 'Soya Bean Milk', 'Almond Milk', 'Lemon Tea', 'Chrysanthemum Tea'}
     }
+    submenuSymbol = "~" # We use this symbol in the callback data to identify the submenu.
+    configureDrink = ["Iced", "No Ice", "Hot"] # Generic configurations of drinks which apply to everything, last step of the order
+    configureSymbol = "!" # We use this symbol in the callback data to identify the configure menu, which is the end of the order
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         # Container for orders
-        self.orders = dict()
+        self.orders = dict() # Lookup for group->user->list of orders
+        self.activeGroup = dict() # Lookup for user->current group
 
     def _createOrderGroup(self, groupid):
         self.orders[groupid] = dict()
 
     def _joinOrderGroup(self, groupid, userid):
-        self.orders[groupid][userid] = None
+        self.orders[groupid][userid] = list()
+        self.activeGroup[userid] = groupid
 
     def _addInterfaceHandlers(self):
         super()._addInterfaceHandlers()
@@ -73,14 +78,14 @@ class OrderInterface:
 
             await context.bot.send_message(
                 chat_id=update.effective_chat.id,
-                text="Starting a new group order.. Tell your friends to join %d!" % (update.effective_chat.id)
+                text="Starting a new group order.. Tell your friends to /join %d! Add your own order with /order." % (update.effective_chat.id)
             )
         
         # Otherwise tell them the order already exists
         else:
             await context.bot.send_message(
                 chat_id=update.effective_chat.id,
-                text="You already have an active order! Tell your friends to join %d!" % (update.effective_chat.id)
+                text="You already have an active order! Tell your friends to /join %d! Add your own order with /order." % (update.effective_chat.id)
             )
 
     async def join(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -88,11 +93,8 @@ class OrderInterface:
         groupid = int(context.args[0])
         if groupid in self.orders:
             # If not yet in the group then join it
-            if update.effective_chat.id not in self.orders[groupid]:
-                self.orders[groupid][update.effective_chat.id] = None
-
-                # DEBUG CHECK
-                print(self.orders)
+            if update.effective_user.id not in self.orders[groupid]:
+                self._joinOrderGroup(groupid, update.effective_user.id)
 
                 await context.bot.send_message(
                     chat_id=update.effective_chat.id,
@@ -113,21 +115,67 @@ class OrderInterface:
             )
 
     async def order(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
-        keyboard = [
-            [
-                InlineKeyboardButton("Option 1", callback_data="1"),
-                InlineKeyboardButton("Option 2", callback_data="2"),
-            ],
-            [InlineKeyboardButton("Option 3", callback_data="3")],
-        ]
+        # Check if the user is in a group first
+        if update.effective_user.id not in self.activeGroup:
+            await context.bot.send_message(
+                chat_id=update.effective_chat.id,
+                text="You are not in a group! Join a group first with /join or start a group with /start."
+            )
+            return
 
-        reply_markup = InlineKeyboardMarkup(keyboard)
-
+        reply_markup = self._generateKeyboard()
 
         await update.message.reply_text(
             "What would you like to order?",
             reply_markup=reply_markup
         )
+
+    def _generateKeyboard(self, key: str=None, subkey: str=None):
+        """
+        Internal method to generate the keyboard for the order interface.
+        """
+        keyboard = []
+
+        # If no key supplied then generate from the top-level menu
+        if key is None:
+            row = []
+            for key in self.menu:
+                row.append(InlineKeyboardButton(key, callback_data=key))
+                if len(row) == 3:
+                    keyboard.append(row)
+                    row = []
+
+            if len(row) > 0:
+                keyboard.append(row)
+
+        # Otherwise we generate from the submenu
+        elif subkey is None:
+            row = []
+            for subkey in self.menu[key]:
+                row.append(InlineKeyboardButton(subkey, callback_data=key+self.submenuSymbol+subkey))
+                if len(row) == 3:
+                    keyboard.append(row)
+                    row = []
+
+            if len(row) > 0:
+                keyboard.append(row)
+
+        # Otherwise generate the final configuration menu
+        else:
+            row = []
+            for config in self.configureDrink:
+                row.append(
+                    InlineKeyboardButton(
+                        config,
+                        callback_data=key+self.submenuSymbol+subkey+self.configureSymbol+config
+                    )
+                )
+            keyboard.append(row)
+            
+        return InlineKeyboardMarkup(keyboard)
+    
+    def _prettifyOrderString(self, querydata: str):
+        return querydata.replace(self.submenuSymbol, " ").replace(self.configureSymbol, ", ")
 
     async def button(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         """Parses the CallbackQuery and updates the message text."""
@@ -137,20 +185,60 @@ class OrderInterface:
         # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
         await query.answer()
 
-        await query.edit_message_text(text=f"Selected option: {query.data}")
+        # Are we at the configure menu?
+        if self.configureSymbol in query.data:
+            # Complete the order
+            activeGroup = self.activeGroup[update.effective_user.id]
+            self.orders[activeGroup][update.effective_user.id].append(
+                self._prettifyOrderString(query.data)
+            )
+            print(self.orders)
 
-        # await update.message.edit_reply_markup(reply_markup=self.menu[query.data])
-        # Maybe this is how you edit the keyboard?
+            await query.edit_message_text(text=f"{self._prettifyOrderString(query.data)} has been added!")
+
+        # Are we in a submenu?
+        elif self.submenuSymbol in query.data:
+            # Get the key and subkey
+            key, subkey = query.data.split(self.submenuSymbol)
+
+            # Generate a configure keyboard
+            reply_markup = self._generateKeyboard(key=key, subkey=subkey)
+
+            await update.callback_query.edit_message_reply_markup(reply_markup=reply_markup)
+            
+        # Is this the top menu?
+        else:
+            # Generate the submenu keyboard
+            reply_markup = self._generateKeyboard(key=query.data)
+
+            await update.callback_query.edit_message_reply_markup(reply_markup=reply_markup)
 
     async def close(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
-        self.orders.pop(update.effective_chat.id, None)
+        # Collate the orders
+        collated = self._collate(self.orders[update.effective_user.id])
+
+        # Then pop the order
+        self.orders.pop(update.effective_user.id, None)
 
         print(self.orders)
 
         await context.bot.send_message(
             chat_id=update.effective_chat.id,
-            text="Closing this group order..."
+            text="Closing this group order...\n%s" % (
+                collated
+            )
         )
+
+    def _collate(self, orders: dict):
+        return "EMPTY"
+    
+    def _closeOrderGroup(self, groupid):
+        # Pop the active group for each of the users in the group
+        for userid in self.orders[groupid]:
+            self.activeGroup.pop(userid, None)
+
+        # Then pop the group itself from orders dictd
+        self.orders.pop(groupid, None)
 
 
 class OrderBot(OrderInterface, cbi.ControlInterface, cbi.StatusInterface, cbi.BotContainer):
